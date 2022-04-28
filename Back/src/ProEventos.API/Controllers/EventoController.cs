52848142ea7346e76c7beb4deea2f681b7d541a3
bufaonanet <douglas.bufaonanet@outlook.com>
2022-04-28 12:26:08 -0300

    Adicionando contexto e migrations

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -10,42 +11,25 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private readonly IEnumerable<Evento> _evento;
-        public EventoController()
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-            _evento = new Evento[]
-            {
-                new Evento {
-                    Id = 1,
-                    Tema ="Angular 12 e .net 5",
-                    Local ="Guanhães",
-                    Lote = "1º Lote",
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                    QtdPessoas = 250
-                },
-                 new Evento {
-                    Id = 2,
-                    Tema ="Novidades .net core",
-                    Local ="Belo Horizonte",
-                    Lote = "2º Lote",
-                    DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
-                    QtdPessoas = 50
-                }
-            };
+            _context = context;
+
         }
 
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> Get(int id)
+        public Evento Get(int id)
         {
-            return _evento
-                .Where(e => e.Id == id);
+            return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
         }
     }
 }
